aa81a321e7742ff63307368ea70431314235419e
Haider Mirza <x7and7@gmail.com>
2021-12-13 20:03:07 +0000

    Update

--- javascript/index.js
+++ javascript/index.js
@@ -21,4 +21,15 @@ const navSlide = () => {
 	});
 }
 
+// News toggle
+// const foldNews = () => {
+// 	const news = document.querySelector('.news h2');
+// 	const links = document.querySelector('.old-news a');
+
+// 	news.addEventListener('click', () => {
+// 		links.classList.toggle('old-news-active');
+// 	});
+// }
+
+// foldNews();
 navSlide();
