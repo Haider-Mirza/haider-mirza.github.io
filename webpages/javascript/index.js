a101aa31011e0152fdb15d72d0904fad88ef64eb
Haider Mirza <x7and7@gmail.com>
2021-12-17 17:06:37 +0000

    Added some cool effects

---
+++ webpages/javascript/index.js
@@ -0,0 +1,24 @@
+const inputTest = document.querySelector(".inputTest");
+const buttonTest = document.querySelector(".buttonTest");
+
+buttonTest.addEventListener("click", function() {
+	if (inputTest.value == 'Haider' || inputTest.value == `haider`) {
+		console.log(`Permission Given`)
+	} else {
+		console.log(`You are not allowed in!`)
+	}
+});
+
+var i = 0;
+var txt = 'Linux developer, 3D artist and Emacs devotee.'; /* The text */
+var speed = 50; /* The speed/duration of the effect in milliseconds */
+
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("demo").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
+
+typeWriter();
