622aeb30cd4491efd6ef06b26880cefea0856cd4
Haider Mirza <x7and7@gmail.com>
2022-02-18 12:54:29 +0000

    test

---
+++ publish.el
@@ -0,0 +1,63 @@
+;; Set the package installation directory so that packages aren't stored in the
+;; ~/.emacs.d/elpa path.
+(require 'package)
+(setq package-user-dir (expand-file-name "./.packages"))
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+                         ("elpa" . "https://elpa.gnu.org/packages/")))
+
+;; Initialize the package system
+(package-initialize)
+(unless package-archive-contents
+  (package-refresh-contents))
+
+;; Install dependencies
+(package-install 'htmlize)
+
+;; Load the publishing system
+(require 'ox-publish)
+
+;; Customize the HTML output
+(setq org-html-validation-link nil            ;; Don't show validation link
+      org-html-head-include-scripts nil       ;; Use our own scripts
+      org-html-metadata-timestamp-format "%Y-%m-%d"
+      org-html-html5-fancy nil
+      org-html-htmlize-output-type 'css
+      org-html-self-link-headlines t
+      org-html-validation-link nil
+      org-html-doctype "html5"
+      org-html-head-include-default-style nil ;; Use our own styles
+      org-html-head "<link rel=\"stylesheet\" href=\"https://www.haider.gq/base/stylesheet.css\"/>")
+
+
+(setq org-publish-use-timestamps-flag t
+      org-publish-timestamp-directory "./.org-cache/"
+      org-export-with-section-numbers nil
+      org-export-use-babel nil
+      org-export-with-smart-quotes t
+      org-export-with-sub-superscripts nil
+      org-export-with-tags 'not-in-toc
+      org-export-with-toc t)
+
+
+;; Define the publishing project
+(setq org-publish-project-alist
+      (list
+       (list "org-site:main"
+             :recursive t
+             :base-directory "./content"
+             :publishing-function 'org-html-publish-to-html
+             :publishing-directory "./public"
+             :with-author nil           ;; Don't include author name
+             :with-creator t            ;; Include Emacs and Org versions in footer
+             :with-toc nil                ;; Include a table of contents
+             :section-numbers nil       ;; Don't include section numbers
+             :with-timestamps t
+             :time-stamp-file nil)))    ;; Don't include time stamp in file
+
+;; We're using Git, we don't need no steenking backups
+(setq make-backup-files nil)
+
+;; Generate the site output
+(org-publish-all t)
+
+(message "Build complete!")
