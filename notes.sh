66aae549fef23de988e2c4ea9f3290c78aeee206
Haider Mirza <x7and7@gmail.com>
2022-03-31 20:57:56 +0100

    Testing

--- notes.sh
+++ notes.sh
@@ -6,7 +6,6 @@ myNotesDir="./content/notes"
 echo -e "Cloning Notes Git Repository\n"
 rm -rf $myNotesDir
 git clone https://github.com/Haider-Mirza/Notes.git ./content/notes
-pwd
 
 cd $myNotesDir
 
@@ -14,7 +13,7 @@ echo -e "Deleting Useless Files\n"
 rm -rf README.org
 rm -rf .git/
 
-echo "Renaming Notes"
-for i in $( ls -p | grep -v / ); do
-mv "${i}" "${i/*-/}"
-done
+# echo "Renaming Notes"
+# for i in $( ls -p | grep -v / ); do
+# mv "${i}" "${i/*-/}"
+# done
