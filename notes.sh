3afadd98c6063f057203387ca86538f9d3be1772
Haider Mirza <x7and7@gmail.com>
2022-05-07 19:38:24 +0100

    Update notes

---
+++ notes.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# Variables
+myNotesDir="./content/notes"
+
+echo -e "Cloning Notes Git Repository\n"
+rm -rf $myNotesDir
+git clone https://github.com/Haider-Mirza/Notes.git ./content/notes
+
+cd $myNotesDir
+
+echo -e "Deleting Useless Files\n"
+rm -rf README.org
+rm -rf .git/
+
+echo "Renaming Notes"
+for i in $( ls -p | grep -v / ); do
+mv "${i}" "${i/*-/}"
+done
