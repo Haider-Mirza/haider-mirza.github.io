43c3e542f85c30672f9815d425bcef15e7e90f70
Haider Mirza <x7and7@gmail.com>
2022-05-15 10:54:49 +0100

    Testing

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 # Run the script
-emacs --batch -q --load publish.el --funcall my/publish-all
+# emacs --batch -q --load publish.el --funcall my/publish-all
+emacs --batch -q -l publish.el -f my/publish-all
