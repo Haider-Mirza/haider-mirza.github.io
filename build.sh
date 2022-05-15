5ab9532732de10ce511b7e23bc022f4801b5addc
Haider Mirza <x7and7@gmail.com>
2022-05-15 16:36:20 +0100

    Build.sh, Publish.el: Back to Github Pages build system

--- build.sh
+++ build.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 # Run the script
-# emacs --batch -q --load publish.el --funcall my/publish-all
-emacs --batch -q -l publish.el -f my/publish-all
+emacs --batch -q --load publish.el --funcall my/publish-all
